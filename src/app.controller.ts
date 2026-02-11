e27594c9027b94ae5d7a81c59342b3800a263029
bvminh-dev <bvminh.dev@gmail.com>
2026-02-11 18:17:31 +0700

    Enhance AppController with Swagger documentation and public access
    - Added Swagger decorators for API documentation, including `@ApiTags`, `@ApiOperation`, and `@ApiOkResponse`.
    - Refactored the root endpoint to provide a structured response indicating server status.
    - Introduced a public access decorator to the endpoint for improved security management.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Public } from './common/decorators/public.decorator.js';
 
+@ApiTags('App')
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+  @Public()
   @Get()
-  root() {
-    return 'hello world';
+  @ApiOperation({ summary: 'Check server status' })
+  @ApiOkResponse({ description: 'Server is running' })
+  getServerStatus() {
+    return { message: 'Server is running' };
   }
 }
